7f5f5cd578633fbcf0dae17abd3a93b25da7f8d7
Beyzanur KESKİN <beyzanrkeskinn94@gmail.com>
2024-08-17 22:38:09 +0300

    added to dicegame

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,24 @@
 import { Component } from "react";
+import { useState } from "react";
 import "./App.css";
 
 function App() {
+  //State update
+  const [inputValue, setInputValue] = useState("Player 1");
+  //input update
+  const handleInputChange = (value) => {
+    setInputValue(value);
+  };
+
   return (
     <>
       <div>
-        <h1>Drag</h1>
         <div>
-          <RoolDice />
+          <PlayerInput
+            onInputChange={handleInputChange}
+            playerName={inputValue}
+          />
+          <RoolDice inputValue={inputValue} />
         </div>
       </div>
     </>
@@ -15,18 +26,19 @@ function App() {
 }
 export default App;
 
+//show dice picture
 export class Dice extends Component {
   render() {
     let cls = `./images/${this.props.img}`;
     return (
       <div>
-        <h1>Player1</h1>
         <img src={cls} />
       </div>
     );
   }
 }
 
+//rolls the dice and calculates the results
 export class RoolDice extends Component {
   static defaultProps = {
     sides: [
@@ -38,16 +50,61 @@ export class RoolDice extends Component {
       "dice6.png",
     ],
   };
-  state = { dice1: "dice1.png", dice2: "dice2.png", rolling: false };
+  state = {
+    dice1: "dice1.png",
+    dice2: "dice2.png",
+    rolling: false,
+    winner: "",
+  };
+
+  // Roll the dice and start animation
   roll = () => {
-    const newDice1 =
-      this.props.sides[Math.floor(Math.random() * this.props.sides.length)];
-    const newDice2 =
-      this.props.sides[Math.floor(Math.random() * this.props.sides.length)];
-    this.setState({ dice1: newDice1, dice2: newDice2, rolling: true });
+    this.setState({ rolling: true, winner: "" });
+
+    // Zarları her 100ms'de bir değiştir
+    const rollingInterval = setInterval(() => {
+      const newDice1 =
+        this.props.sides[Math.floor(Math.random() * this.props.sides.length)];
+      const newDice2 =
+        this.props.sides[Math.floor(Math.random() * this.props.sides.length)];
+      this.setState({ dice1: newDice1, dice2: newDice2 });
+    }, 100);
     setTimeout(() => {
-      this.setState({ rolling: false });
+      clearInterval(rollingInterval);
+      this.setState({ rolling: false }, this.determineWinner);
     }, 3000);
+  };
+
+  //Determines the winner and assigns it to the state
+  determineWinner = () => {
+    const { dice1, dice2 } = this.state;
+    const dice1Value = parseInt(dice1.replace("dice", "").replace(".png", ""));
+    const dice2Value = parseInt(dice2.replace("dice", "").replace(".png", ""));
+
+    let winner;
+    if (dice1Value > dice2Value) {
+      winner = (
+        <>
+          <h2>{this.props.inputValue} Wins!</h2>
+          <p>Player 2 Loses</p>
+        </>
+      );
+    } else if (dice1Value < dice2Value) {
+      winner = (
+        <>
+          <h2>Player 2 Wins!</h2>
+          <p>{this.props.inputValue} Loses </p>
+        </>
+      );
+    } else {
+      winner = (
+        <h1>
+          Draw! <i className="fas fa-handshake"></i>
+        </h1>
+      );
+    }
+
+    this.setState({ winner });
   };
   render() {
     return (
@@ -67,6 +124,39 @@ export class RoolDice extends Component {
             <i className="fa-solid fa-shuffle"></i>
           )}{" "}
         </button>
+
+        {this.state.winner && <div className="winner">{this.state.winner}</div>}
+      </div>
+    );
+  }
+}
+
+//Used to get name from user
+export class PlayerInput extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      inputValue: props.playerName || "Player 1",
+    };
+  }
+
+  handleChange = (event) => {
+    const newValue = event.target.value;
+    this.setState({ inputValue: newValue });
+    this.props.onInputChange(newValue);
+  };
+  render() {
+    return (
+      <div>
+        <input
+          type="text"
+          onChange={this.handleChange}
+          placeholder="İsminizi giriniz..."
+        />
+        <div className="player matemasie-regular">
+          <p> {this.state.inputValue}</p>
+          <p>Player 2</p>
+        </div>
       </div>
     );
   }
